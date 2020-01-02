3a8560f92e30d4f54848cc32e04ae2b3b60deffd
Your Name <you@example.com>
2020-01-03 00:47:06 +0100

    first automount camera

--- Glomod.lua
+++ Glomod.lua
@@ -13,7 +13,7 @@ function GlomodOnload(self)
   self:RegisterEvent("UNIT_MODEL_CHANGED");
   self:RegisterEvent("PLAYER_CONTROL_GAINED");  
   self:RegisterEvent("PLAYER_CONTROL_LOST");  
-  --self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
+  self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
   PlayerFrame:SetScript('OnEnter', function() ShowAll() end)
   PlayerFrame:SetScript('OnLeave', function() CheckHide() end)
   TargetFrame:SetScript('OnEnter', function() ShowAll() end)
@@ -29,14 +29,14 @@ function GlomodOnload(self)
 end
 
 function CheckHide()
-    if inCombat == false then
-      C_Timer.After(3, function() HideAll() end)
-    end
+    if inCombat == false then C_Timer.After(3, function() HideAll() end) end
 end
 
 function HideAll()
   if inCombat or targeting or PlayerFrame:IsMouseOver() or TargetFrame:IsMouseOver() or
-    MainMenuBar:IsMouseOver() or MicroButtonAndBagsBar:IsMouseOver() or ChatFrame1:IsMouseOver() then return end
+    MainMenuBar:IsMouseOver() or MicroButtonAndBagsBar:IsMouseOver() or ChatFrame1:IsMouseOver() then 
+        return 
+  end
   fade=fade-0.1
   --print(fade)
   FadeAll()
@@ -44,15 +44,28 @@ function HideAll()
 end
 
 function FadeAll()
-  PlayerFrame:SetAlpha(fade); TargetFrame:SetAlpha(fade);MainMenuBar:SetAlpha(fade);
-  MicroButtonAndBagsBar:SetAlpha(fade); ChatFrame1:SetAlpha(fade);
+  PlayerFrame:SetAlpha(fade); 
+  TargetFrame:SetAlpha(fade);
+  MainMenuBar:SetAlpha(fade);
+  MicroButtonAndBagsBar:SetAlpha(fade); 
+  ChatFrame1:SetAlpha(fade);
 end
 
 function ShowAll()
-  fade=1; FadeAll();
+  fade=1; 
+  FadeAll();
 end
 
-function GlomodEventHandler(self, event, ...)
+function CheckMount()
+    if IsMounted() then 
+        SetView(2) 
+    else 
+        if inCombat == false then SetView(1) end
+        --SetView(1)
+    end
+end
+
+function GlomodEventHandler(self, event, arg1, arg2, arg3)
   --print("EVENT TRIGGERED : " .. event);
   -- utiliser la commande /fstack en jeu pour identifier les élements de l'interface wow
   if event == 'PLAYER_REGEN_DISABLED' then 
@@ -61,6 +74,9 @@ function GlomodEventHandler(self, event, ...)
   elseif event == 'PLAYER_REGEN_ENABLED' then
     inCombat = false; 
     CheckHide()
+  elseif event == 'UNIT_SPELLCAST_SUCCEEDED' then
+    print ('SPELL'..arg1..' '..arg3); 
+    C_Timer.After(1, function() CheckMount() end)
   elseif event == 'PLAYER_TARGET_CHANGED' then
     if UnitExists("target") then
       ShowAll(); 
